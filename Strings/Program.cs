a075875183f9b39d116e32767e6c0afe8eac3009
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-06-29 14:33:25 -0400

    practice

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -14,11 +14,30 @@ namespace Strings
             //int lengthOfName= firstName.Length;
             //Console.WriteLine(lengthOfName);
 
-            //string escapeString = "I want a tab after this line \t I want a new line without WriteLine\n Hi";
-            //String ecsapeString2 = "I want o show a backslash here\\ Hey what's that noise? \a";
-            //Console.WriteLine(escapeString);
-            //Console.WriteLine(ecsapeString2);
+            string escapeString = "I want a tab after this line \t I want a new line without WriteLine\n Hi";
+            String ecsapeString2 = "I want o show a backslash here\\ Hey what's that noise? \a";
+            Console.WriteLine(escapeString);
+            Console.WriteLine(ecsapeString2);
+            Console.WriteLine(escapeString + ecsapeString2);
+            //Console.WriteLine("User 1, please type something.");
+            //string userOneInput = Console.ReadLine();
 
+            //Console.WriteLine("User 2, please type something.");
+            //string userTwoImput = Console.ReadLine();
+
+            //if(userOneInput .Equals(userTwoImput))
+            //{
+            //    Console.WriteLine("No!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yes!");
+            //}
+            //string startOfSentence = "My best friend's name is ";
+            //string endOfSentence = "Stephanie.";
+            //string fullSentence = startOfSentence = endOfSentence;
+            //Console.WriteLine(fullSentence);
+            //Console.WriteLine("my friend {0} is awsome", endOfSentence);
         }
     }
 }
