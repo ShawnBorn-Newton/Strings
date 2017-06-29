a466d819c6ba2452e6e55c620b890f6ddfcd0f04
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-06-29 14:28:49 -0400

    practice

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -10,6 +10,15 @@ namespace Strings
     {
         static void Main(string[] args)
         {
+            //string firstName = "Shawn";
+            //int lengthOfName= firstName.Length;
+            //Console.WriteLine(lengthOfName);
+
+            //string escapeString = "I want a tab after this line \t I want a new line without WriteLine\n Hi";
+            //String ecsapeString2 = "I want o show a backslash here\\ Hey what's that noise? \a";
+            //Console.WriteLine(escapeString);
+            //Console.WriteLine(ecsapeString2);
+
         }
     }
 }
